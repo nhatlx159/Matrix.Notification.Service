cbbd2f36a43f1eb888612e453f1638721cf30dde
Vermillion <nhatlx159@gmail.com>
2024-01-19 21:10:34 +0700

    build authNotification route

--- routes/authNotification.js
+++ routes/authNotification.js
@@ -2,8 +2,11 @@ const authController = require('../controllers/authController');
 
 const router = require('express').Router();
 
+// REGISTER
 router.post("/register", authController.registerSendCode);
+// CONFIRM REGISTER
 router.post("/comfirmregister", authController.confirmRegisterVerifyCode);
+// LOGIN IN NEW DEVICE
 router.post("/loginonnewdevice", authController.confirmLoginOnNewDevice);
 
 module.exports = router
